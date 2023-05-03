a65d3fec0a730421357d80f1090713b88882c128
isabellexvr <isabellexavier59@gmail.com>
2023-05-03 19:49:43 -0300

    fix: loggeduser decorator type

--- src/credentials/credentials.controller.ts
+++ src/credentials/credentials.controller.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from 'src/guards/auth.guard';
 import { CredentialsService } from './credentials.service';
 import { CreateCredentialDTO } from './dtos/create-credential.dto';
 import { LoggedUser } from 'src/decorators/logged-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('credentials')
 export class CredentialsController {
@@ -10,25 +11,25 @@ export class CredentialsController {
 
     @UseGuards(AuthGuard)
     @Post()
-    async createNewCredential(@Body() body: CreateCredentialDTO, @LoggedUser() user) {
+    async createNewCredential(@Body() body: CreateCredentialDTO,@LoggedUser() user: User) {
         return await this.credentialsService.createCredential(body, user.id)
     }
 
     @UseGuards(AuthGuard)
     @Get("all")
-    async findAllCredentials(@LoggedUser() user) {
+    async findAllCredentials(@LoggedUser() user: User) {
         return await this.credentialsService.findAllUserCredentials(user.id);
     }
 
     @UseGuards(AuthGuard)
     @Get("unique/:credentialId")
-    async findCredentialById(@Param("credentialId", ParseIntPipe) credentialId: number, @LoggedUser() user) {
+    async findCredentialById(@Param("credentialId", ParseIntPipe) credentialId: number, @LoggedUser() user: User) {
         return await this.credentialsService.findById(credentialId, user.id);
     }
 
     @UseGuards(AuthGuard)
     @Delete("delete/:credentialId")
-    async deleteCredential(@Param("credentialId", ParseIntPipe) credentialId: number, @LoggedUser() user){
+    async deleteCredential(@Param("credentialId", ParseIntPipe) credentialId: number, @LoggedUser() user: User){
         return await this.credentialsService.deleteOne(credentialId, user.id);
     }
 }
